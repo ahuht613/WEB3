a10222b194d68df605699c4254da6050e55c70a5
TranThuHa <tranthuha06012003@gmail.com>
2024-05-25 02:52:05 -0700

    ok

---
+++ includes/leftbar.php
@@ -0,0 +1,62 @@
+<div class="left-sidebar bg-black-300 box-shadow ">
+                        <div class="sidebar-content">
+                            <div class="user-info closed">
+                                <img src="http://placehold.it/90/c2c2c2?text=User" alt="John Doe" class="img-circle profile-img">
+                                <h6 class="title">Trần Thu Hà</h6>
+                                <small class="info">Quản lí</small>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Danh mục chính</span>
+                                    </li>
+                                    <li>
+                                        <a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>Trang chủ</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class=""></span>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Lớp</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-class.php"><i class="fa fa-bars"></i> <span>Tạo lớp</span></a></li>
+                                            <li><a href="manage-classes.php"><i class="fa fa fa-server"></i> <span>quản lý lớp</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+  <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Môn học</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-subject.php"><i class="fa fa-bars"></i> <span>Tạo môn học</span></a></li>
+                                            <li><a href="manage-subjects.php"><i class="fa fa fa-server"></i> <span>Quản lý môn học</span></a></li>
+                                           <li><a href="add-subjectcombination.php"><i class="fa fa-newspaper-o"></i> <span>Thêm môn học cho lớp</span></a></li>
+                                           <a href="manage-subjectcombination.php"><i class="fa fa-newspaper-o"></i> <span>Quản lý danh sách môn theo lớp</span></a></li>
+                                        </ul>
+                                    </li>
+   <li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Học sinh</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-students.php"><i class="fa fa-bars"></i> <span>Thêm học sinh</span></a></li>
+                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>Quản lý học sinh</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+<li class="has-children">
+                                        <a href="#"><i class="fa fa-info-circle"></i> <span>Kết quả</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-result.php"><i class="fa fa-bars"></i> <span>Nhập kết quả</span></a></li>
+                                            <li><a href="manage-results.php"><i class="fa fa fa-server"></i> <span>Thống kê kết quả</span></a></li>
+                                           
+                                        </ul>
+                                        <li><a href="change-password.php"><i class="fa fa fa-server"></i> <span>Đổi mật khẩu</span></a></li>
+                                           
+                                    </li>
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
